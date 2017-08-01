493c60c06f462ba6170b05386495b38c90d40392
crowebot <30629985+crowebot@users.noreply.github.com>
2017-08-01 10:58:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text) && request.sender_type="user") {
     this.res.writeHead(200);
     postMessage(cool());
     postMessage("Beep Bop " +request.name+ " Sucks");
