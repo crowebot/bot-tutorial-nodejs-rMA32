3fe621e2c4505fe266462e83c386d2b7b8eb0a98
crowebot <30629985+crowebot@users.noreply.github.com>
2017-11-22 14:05:27 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
-  if(request.user_id == '9469129') {
+  if(request.user_id == '9469129' && Math.random()>0.75) {
    // if(request.user_id && botRegex.test(request.user_id)) {
     this.res.writeHead(200);
     postMessage("Good one Momo");
