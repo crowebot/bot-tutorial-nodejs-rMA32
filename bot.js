a00882f867c7e5b9be1c309454a5e00f8649d23d
crowebot <30629985+crowebot@users.noreply.github.com>
2017-11-22 15:13:19 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,78 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
-var weather;
 
 var botID = process.env.BOT_ID;
-var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
-var apiKey = '&APPID=fed9ecac1c3e3875fdd6c89cb5afe97b';
-var units = '&units=metric';
-var input ='london';
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /l/gi; botRegexNB= /northbrook/gi;
+  var request = JSON.parse(this.req.chunks[0]),//momo user id 9469129
+      botRegex = /7522678/gi; botRegexNB= /northbrook/gi;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
-  if(request.text && botRegex.test(request.text)) {
+  if(request.user_id == '9469129' && Math.random()>0.75) {
+   // if(request.user_id && botRegex.test(request.user_id)) {
     this.res.writeHead(200);
-     postMessage(request.name+ " did someone say chicken?");
- //   Start Weather
-  var url = api + input + apiKey + units;
-    function weatherAsk() {
-  var url = api + input + apiKey + units;
-  loadJSON(url, gotData);
-}
-
-function gotData(data) {
-  weather = data;
-}
-
-  var temp = weather.main.temp;
-    
-    // End Weather
-    postMessage(cool());
-    postMessage(temp);
-   
+    postMessage("Good one Momo");
     this.res.end();
   }
-  
+  else if(request.user_id == '7522682') {
+   // if(request.user_id && botRegex.test(request.user_id)) {
+    this.res.writeHead(200);
+    postMessage("Graduate");
+    this.res.end();
+  }
+  else if(request.user_id != '7522682') {
+   // if(request.user_id && botRegex.test(request.user_id)) {
+    this.res.writeHead(200);
+   addBot(request.text);
+    this.res.end();
+  }
   else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
+
+function addBot(name) {
+  var body, botReq;
+
+  //botResponse = response
+//POST /groups/:group_id/members/:membership_id/remove
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots',
+    method: 'POST'
+  };
+
+  body = {
+            {
+          "bot" : {
+            "name" : name,
+            "group_id" : "17583802"
+          }
+        }
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 
 function postMessage(response) {
   var botResponse,options, body, botReq;
