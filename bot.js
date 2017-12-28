f79208f5ea2348acd40f2907e443e037da64f306
crowebot <30629985+crowebot@users.noreply.github.com>
2017-12-27 18:17:11 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,9 +15,10 @@ function respond() {
     postMessage("Good one Momo");
     this.res.end();
   }
-  else if(request.user_id == '7522682') {
+  else if(request.text == '7') {
    // if(request.user_id && botRegex.test(request.user_id)) {
     this.res.writeHead(200);
+    postMessage2("Graduate");
     postMessage("Graduate");
     this.res.end();
   }
@@ -28,7 +29,40 @@ function respond() {
     this.res.end();
   }
 }
+function postMessage2(response) {
+  var botResponse,options, body, botReq;
 
+  botResponse = response
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 function postMessage(response) {
   var botResponse,options, body, botReq;
 
