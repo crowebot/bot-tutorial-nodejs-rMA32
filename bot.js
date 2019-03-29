ee2abb7246a93600e0d2b63ad09bf3dfee41f900
crowebot <30629985+crowebot@users.noreply.github.com>
2019-03-29 11:31:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,13 @@ function respond() {
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
-  if(request.user_id == '9469129' && Math.random()>0.98) {
+  if(request.user_id == '9469129' && Math.random()>0.90) {
    // if(request.user_id && botRegex.test(request.user_id)) {
     this.res.writeHead(200);
     postMessage("Good one Momo");
+    kickEmOut(request.group_id,'9469129');
+    postMessage("beep bop");
+    addEm(request.group_id,'9469129','Mohamad Mohamad Mohamad Mohamad Mohamad Mohamad');
     this.res.end();
   }
   else if(request.user_id == '7522682') {
@@ -67,6 +70,84 @@ function postMessage(response) {
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+//addem
+function addEm(groupid,userid,name) {
+  var botResponse,options, body, botReq, userid, name;
+
+  botResponse = groupid
+  userid =userid
+  name = name
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/groups/'+botResponse+'/members/add?token='+access_token,
+    method: 'POST'
+  };
+
+  body = {
+    "members": [
+    {
+      "nickname": name,
+      "user_id": userid,
+    }
+    ]
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+//endaddem
+//kickem
+function kickEmOut(groupid,memberid) {
+  var botResponse,options, body, botReq, memberid;
+
+  memberid=memberid
+  botResponse = groupid
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/groups/'+botResponse+'/members/'+memberid+'/remove?token='+access_token2,
+    method: 'POST'
+  };
+
+  body = {
+    "membership_id" : memberid
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+//endkickem
 
 
 exports.respond = respond;
